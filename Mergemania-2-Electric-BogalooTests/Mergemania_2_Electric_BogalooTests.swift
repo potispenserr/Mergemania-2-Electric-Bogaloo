4c0af7108dd86433a813a766de2c85cfbb032853
Benjamin Vesterlund <benves-7@student.ltu.se>
2020-10-21 19:30:33 +0200

    Initial Commit

---
+++ Mergemania-2-Electric-BogalooTests/Mergemania_2_Electric_BogalooTests.swift
@@ -0,0 +1,34 @@
+//
+//  Mergemania_2_Electric_BogalooTests.swift
+//  Mergemania-2-Electric-BogalooTests
+//
+//  Created by admin on 2020-10-21.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import XCTest
+@testable import Mergemania_2_Electric_Bogaloo
+
+class Mergemania_2_Electric_BogalooTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
