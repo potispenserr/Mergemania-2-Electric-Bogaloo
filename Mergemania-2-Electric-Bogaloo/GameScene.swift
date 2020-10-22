69bcfe8f1d8cdf8ffd5d41dc62ab6a6ef4442608
Benjamin Vesterlund <benves-7@student.ltu.se>
2020-10-22 19:28:30 +0200

    lines work

--- Mergemania-2-Electric-Bogaloo/GameScene.swift
+++ Mergemania-2-Electric-Bogaloo/GameScene.swift
@@ -13,6 +13,8 @@ class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
+    var pointList = [CGPoint]()
+    let shape = CAShapeLayer()
     
     override func didMove(to view: SKView) {
         
@@ -28,13 +30,19 @@ class GameScene: SKScene {
         self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
         
         if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+            spinnyNode.lineWidth = 3
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(180), duration: 1)))
             spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
                                               SKAction.fadeOut(withDuration: 0.5),
                                               SKAction.removeFromParent()]))
         }
+        shape.opacity = 0.5
+        shape.lineWidth = 2
+        shape.lineJoin = CAShapeLayerLineJoin.miter
+        shape.strokeColor = UIColor(hue: 0.786, saturation: 0.79, brightness: 0.53, alpha: 1.0).cgColor
+        shape.fillColor = UIColor(hue: 0.786, saturation: 0.15, brightness: 0.89, alpha: 1.0).cgColor
+
     }
     
     
@@ -63,27 +71,63 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        pointList.removeAll()
         if let label = self.label {
             label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for t in touches {
+            self.touchDown(atPoint: t.location(in: self))
+            pointList.append(t.location(in: self))
+           
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for t in touches {
+            self.touchMoved(toPoint: t.location(in: self))
+            pointList.append(t.location(in: self))
+            
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for t in touches {
+            self.touchUp(atPoint: t.location(in: self))
+            pointList.append(t.location(in: self))
+            
+        }
+        
+        
+        let path = UIBezierPath()
+        path.move(to: pointList.first!)
+        for points in pointList{
+            path.addLine(to: points)
+            print("Adding point")
+            print(points)
+            
+        }
+        path.close()
+        shape.path = path.cgPath
+        print("Shape path")
+        print(shape.path ?? "no")
+        self.addChild(SKShapeNode(path: path.cgPath))
+        
+
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for t in touches {
+            self.touchUp(atPoint: t.location(in: self))
+            pointList.append(t.location(in: self))
+            
+            
+        }
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
     }
 }
